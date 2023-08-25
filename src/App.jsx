4d774d085c83812cb7cad1d261807ac5e9299b6f
Rasik Abdullah <rasikabdullah@gmail.com>
2023-08-26 02:45:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to blue-700 h-fit shadow-xl'>
+
+      </div>
+    </>
+  )
+}
+
+export default App
