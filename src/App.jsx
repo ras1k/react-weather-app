e9bbab70f395656f4cbca3c79aa9dcc9899df3a7
Rasik Abdullah <rasikabdullah@gmail.com>
2023-08-26 15:28:11 +0600

    added font

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Inputs from './components/Inputs'
+import TimeAndLocation from './components/TimeAndLocation'
 import TopButtons from './components/TopButtons'
 
 function App() {
@@ -10,6 +11,8 @@ function App() {
       <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
         <TopButtons />
         <Inputs />
+        
+        <TimeAndLocation />
       </div>
     </>
   )
