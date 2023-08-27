54795a06aafef2079e8a3bbd86c0b26e83f0da7e
Rasik Abdullah <rasikabdullah@gmail.com>
2023-08-27 14:14:47 +0600

    added search and units

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,27 +1,70 @@
-import { BsSearch } from "react-icons/bs";
-import { FaLocationDot } from "react-icons/fa6";
+import { useState } from "react";
+import { UilSearch, UilLocationPoint } from "@iconscout/react-unicons";
 
-const Inputs = () => {
+const Inputs = ({ setQuery, units, setUnits }) => {
+
+    const [city, setCity] = useState('');
+
+    const handleSearch = () => {
+        if (city !== '') setQuery({ q: city })
+
+    }
+
+    const handleLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                let lat = position.coords.latitude
+                let lon = position.coords.longitude
+
+                setQuery({
+                    lat,
+                    lon,
+                });
+            });
+        }
+        console.log(city);
+    };
+
+    const handleUnitsChange = (e) => {
+        const selectedUnit = e.currentTarget.name
+        if(units !== selectedUnit) setUnits(selectedUnit)
+    }
+
     return (
         <div className="flex flex-row justify-center my-6">
             <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
-                <input type="text"
+                <input
+                    value={city}
+                    onChange={(e) => setCity(e.currentTarget.value)}
+                    type="text"
                     className="text-xl font-semibold p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
                     placeholder="Search for city..."
                 />
-                <BsSearch
-                    className="text-white cursor-pointer transition ease-out hover:scale-150"
+                <UilSearch
+                    size={25}
+                    className="text-white cursor-pointer transition ease-out hover:scale-125"
+                    onClick={handleSearch}
                 />
-                <FaLocationDot
-                    className="text-white cursor-pointer transition ease-out hover:scale-150"
+                <UilLocationPoint
+                    size={25}
+                    className="text-white cursor-pointer transition ease-out hover:scale-125"
+                    onClick={handleLocation}
                 />
             </div >
             <div className="flex flex-row w-1/4 items-center justify-center">
-                <button name="metric" className="text-xl text-white mx-2 cursor-pointer transition ease-out hover:scale-125">
+                <button
+                    name="metric"
+                    className="text-xl text-white mx-2 cursor-pointer transition ease-out hover:scale-125"
+                    onClick={handleUnitsChange}
+                >
                     °C
                 </button>
                 <p className="text-white">|</p>
-                <button name="imperial" className="text-xl text-white mx-2 cursor-pointer transition ease-out hover:scale-125">
+                <button
+                    name="imperial"
+                    className="text-xl text-white mx-2 cursor-pointer transition ease-out hover:scale-125"
+                    onClick={handleUnitsChange}
+                >
                     °F
                 </button>
             </div>
--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -72,7 +72,7 @@ const getFormattedWeatherData = async (searchParams) => {
 const formatToLocalTime = (
     secs,
     zone,
-    format = "cccc, dd LLL YYYY' | Local time: 'hh:mm a"
+    format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a"
 ) => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
 
 const iconUrlFromCode = (code) => `http://openweathermap.org/img/wn/${code}@2x.png` ;
